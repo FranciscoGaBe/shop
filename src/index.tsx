6ed0023b6908efcbe839ebe1f456b9627a01782a
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-04-09 02:04:52 +0200

    Chore: upgraded to React 18
    Switched reactDOM.render to the new way of renderind the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import * as serviceWorker from './serviceWorker';
 import Router from './Router';
 
-ReactDOM.render(
+const container = document.querySelector('#root') as Element;
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
