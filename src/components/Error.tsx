90694816dec259023d657df14d4a5c38c54a9ffa
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-04-20 17:48:08 +0200

    Feat: error page

---
+++ src/components/Error.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Error: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-rose-800 z-10"
+    >
+      <div className="text-center">
+        <p className="text-4xl font-semibold mb-8 text-white px-2">
+          Seems like something went wrong...
+        </p>
+        <button
+          className={`
+              text-2xl font-medium px-4 py-1 rounded
+              bg-white hover:bg-rose-800
+              text-rose-800 hover:text-white
+              transition-color duration-300 ease-in-out
+            `}
+          type="button"
+          onClick={() => { navigate(0); }}
+        >
+          Reload
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
