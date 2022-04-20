05a5fdc96effbe9ed40f309ad32791ea88cd1aa7
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-04-20 16:56:37 +0200

    Feat: Heading component for titles

---
+++ src/components/Heading.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+interface Props {
+  level: 1 | 2 | 3 | 4 | 5 | 6
+}
+
+const classes = {
+  1: '',
+  2: 'text-5xl',
+  3: '',
+  4: '',
+  5: '',
+  6: '',
+};
+
+const Heading: React.FC<Props> = ({ level, children }) => React.createElement(
+  `h${level}`,
+  { className: `inline-block font-bold text-rose-800 mb-8 ${classes[level]}` },
+  <>
+    <span className="block mb-2 px-2">{ children }</span>
+    <motion.div animate={{ width: '6rem' }} className="border-2 border-rose-800" />
+  </>,
+);
+
+export default Heading;
