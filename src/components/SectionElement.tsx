843ac70256ed69de2406afe7d0b4eeb967a05706
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-04-20 17:15:58 +0200

    Fix: animation not triggering on small devices

--- src/components/SectionElement.tsx
+++ src/components/SectionElement.tsx
@@ -1,25 +1,23 @@
 import { motion, Variants } from 'framer-motion';
-import React, { HTMLProps, useState } from 'react';
+import React, { HTMLProps } from 'react';
 
 const SectionElement: React.FC<HTMLProps<HTMLElement>> = ({ className, children }) => {
-  const [shown, setShown] = useState(false);
-
   const variants: Variants = {
-    initial: { opacity: 0, x: `${(Math.floor(Math.random() * 3) - 1) * 1000}px` },
-    inView: () => {
-      if (!shown) setShown(true);
-      return { opacity: 1, x: 0 };
-    },
+    initial: { opacity: 0, x: `${(Math.floor(Math.random() * 3) - 1) * 200}px` },
+    inView: { opacity: 1, x: 0 },
   };
 
   return (
-    <section className={`py-8 ${className} overflow-hidden`}>
+    <section
+      className={`py-8 ${className} overflow-hidden`}
+    >
       <motion.div
         variants={variants}
         initial="initial"
-        animate={shown ? 'inView' : 'initial'}
         whileInView="inView"
-        transition={{ duration: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.3 }}
+        layout
         className="max-w-5xl mx-auto text-center"
       >
         { children }
