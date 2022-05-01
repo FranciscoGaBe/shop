0cb11f99f70df98913ad763d784f7afa0b711f5b
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-05-01 21:23:27 +0200

    Style: Error component

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -6,19 +6,19 @@ const Error: React.FC = () => {
 
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center bg-rose-800 z-10"
+      className="fixed inset-0 flex items-center justify-center"
       role="figure"
       aria-label="error"
     >
       <div className="text-center">
-        <p className="text-4xl font-semibold mb-8 text-white px-2">
+        <p className="text-4xl font-semibold mb-8 text-rose-800 px-2">
           Seems like something went wrong...
         </p>
         <button
           className={`
               text-2xl font-medium px-4 py-1 rounded
-              bg-white hover:bg-rose-800
-              text-rose-800 hover:text-white
+              bg-rose-800 hover:bg-gray-100
+              text-white hover:text-rose-800
               transition-color duration-300 ease-in-out
             `}
           type="button"
