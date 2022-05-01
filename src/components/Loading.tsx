6382e615c2d339f72c73bd2c16e14ada515be017
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-05-01 21:21:37 +0200

    Style: Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,11 +3,11 @@ import logo from '../logo.svg';
 
 const Loading: React.FC = () => (
   <div
-    className="bg-rose-800/90 flex items-center justify-center fixed inset-0 z-10"
+    className="flex items-center justify-center fixed inset-0"
     role="figure"
     aria-label="loading"
   >
-    <div className="text-center text-white">
+    <div className="text-center text-white bg-rose-800 rounded-full relative">
       <img className="w-80 animate-pulse" src={logo} alt="Logo" />
     </div>
   </div>
