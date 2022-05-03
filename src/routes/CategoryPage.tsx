049f80bada15ecf5cd43ec91c1f074e94994d781
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-05-03 19:59:22 +0200

    Chore: refactored CategoryPage

--- src/routes/CategoryPage.tsx
+++ src/routes/CategoryPage.tsx
@@ -1,59 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Filters, { Filter } from '../components/Filters';
+import Filters from '../components/Filters';
 import Heading from '../components/Heading';
 import LoadingHandler from '../components/LoadingHandler';
 import ProductsDisplayer from '../components/ProductsDisplayer';
 import SectionElement from '../components/SectionElement';
 import { useGetProductByCategoryQuery } from '../services/shop';
-import { Product } from '../services/types';
-
-const filters: Filter[] = [
-  {
-    type: 'select',
-    value: '1',
-    name: 'sortBy',
-    options: [
-      { value: '1', text: 'Popularity: high to low' },
-      { value: '2', text: 'Popularity: low to high' },
-      { value: '3', text: 'Price: high to low' },
-      { value: '4', text: 'Price: low to high' },
-      { value: '5', text: 'Rating: high to low' },
-      { value: '6', text: 'Rating: low to hight' },
-    ],
-  },
-  {
-    type: 'text', name: 'name', value: '', placeholder: 'Search by name',
-  },
-];
-
-const sortFunctions: Record<string, (a: Product, b: Product) => number> = {
-  1: (a, b) => (a.rating.count < b.rating.count ? 1 : -1),
-  2: (a, b) => (a.rating.count > b.rating.count ? 1 : -1),
-  3: (a, b) => (a.price < b.price ? 1 : -1),
-  4: (a, b) => (a.price > b.price ? 1 : -1),
-  5: (a, b) => (a.rating.rate < b.rating.rate ? 1 : -1),
-  6: (a, b) => (a.rating.rate > b.rating.rate ? 1 : -1),
-};
+import filters, { filterAndSortProducts, reduceFilters } from '../utils/commonFilters';
 
 const CategoryPage: React.FC = () => {
   const { category } = useParams();
-  const [appliedFilters, setAppliedFilters] = useState<Record<string, string>>(
-    filters.reduce((obj, filter) => ({ ...obj, [filter.name]: filter.value }), {}),
-  );
+  const [appliedFilters, setAppliedFilters] = useState(reduceFilters(filters));
   const {
     data, isLoading, isError, isFetching,
   } = useGetProductByCategoryQuery(category as string);
-  const [sortedProducts, setSortedProducts] = useState<Product[] | undefined>(data);
+  const [sortedProducts, setSortedProducts] = useState(filterAndSortProducts(data, appliedFilters));
 
   useEffect(() => {
-    const { name, sortBy } = appliedFilters;
-    setSortedProducts(
-      (data || []).filter((product) => {
-        if (product.title.toLowerCase().indexOf(name.toLowerCase()) < 0) return false;
-        return true;
-      }).sort(sortFunctions[sortBy]),
-    );
+    setSortedProducts(filterAndSortProducts(data, appliedFilters));
   }, [data, appliedFilters]);
 
   return (
