cee286e2f33cdec493f6939e76293e4e5e11ea7e
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-05-03 14:35:27 +0200

    Feat: added ProductDisplayer to category

--- src/routes/CategoryPage.tsx
+++ src/routes/CategoryPage.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Heading from '../components/Heading';
+import LoadingHandler from '../components/LoadingHandler';
+import ProductsDisplayer from '../components/ProductsDisplayer';
+import SectionElement from '../components/SectionElement';
+import { useGetProductByCategoryQuery } from '../services/shop';
 
 const CategoryPage: React.FC = () => {
   const { category } = useParams();
+  const {
+    data, isLoading, isError, isFetching,
+  } = useGetProductByCategoryQuery(category as string);
 
-  return <div>{ category }</div>;
+  return (
+    <LoadingHandler isLoading={isLoading || isFetching} isError={isError}>
+      { data && (
+        <div>
+          <SectionElement>
+            <Heading level={1}>
+              <span className="capitalize text-4xl">{ category }</span>
+            </Heading>
+            <ProductsDisplayer key={category} products={data} />
+          </SectionElement>
+        </div>
+      ) }
+    </LoadingHandler>
+  );
 };
 
 export default CategoryPage;
